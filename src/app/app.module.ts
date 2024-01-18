4a4b758eb1c51019c72cefc287719dd5ba939128
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-01-18 14:41:27 +0100

    added AngularMaterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { MainscreenComponent } from './mainscreen/mainscreen.component';
 import { ThreadComponent } from './mainscreen/thread/thread.component';
 import { WorkspaceComponent } from './mainscreen/workspace/workspace.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { WorkspaceComponent } from './mainscreen/workspace/workspace.component';
     provideFirebaseApp(() => initializeApp({"projectId":"dabubble-69322","appId":"1:486842154610:web:0631e3885b73bc4e4acceb","storageBucket":"dabubble-69322.appspot.com","apiKey":"AIzaSyC2opRUMbcOUpjD2QPCifl1muUI_7Wf-cw","authDomain":"dabubble-69322.firebaseapp.com","messagingSenderId":"486842154610"})),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideDatabase(() => getDatabase())
+    provideDatabase(() => getDatabase()),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

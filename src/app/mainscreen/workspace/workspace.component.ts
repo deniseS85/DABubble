fb2472adbf849799747ea3cc9bc04304e5323041
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-01-20 18:37:20 +0100

    workspace channel-create-window

--- src/app/mainscreen/workspace/workspace.component.ts
+++ src/app/mainscreen/workspace/workspace.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-workspace',
@@ -6,6 +6,41 @@ import { Component } from '@angular/core';
   styleUrl: './workspace.component.scss'
 })
 export class WorkspaceComponent {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  resetSelection() {
+    const elements = this.el.nativeElement.querySelectorAll('.selectable');
+    elements.forEach((element: HTMLElement) => {
+      this.renderer.removeClass(element, 'selected');
+    });
+  }
+
+  handleClick(event: MouseEvent) {
+    this.resetSelection();
+    
+    const target = event.target as HTMLElement;
+
+    // Überprüfe, ob das geklickte Element oder eines seiner Elternelemente die Klasse 'selectable' hat
+    const selectableElement = this.findClosestSelectable(target);
+
+    if (selectableElement) {
+      this.renderer.addClass(selectableElement, 'selected');
+    }
+  }
+
+  // Hilfsmethode, um das nächste Elternelement mit der Klasse 'selectable' zu finden
+  private findClosestSelectable(element: HTMLElement): HTMLElement | null {
+    while (element) {
+      if (element.classList.contains('selectable')) {
+        return element;
+      }
+      element = element.parentElement!;
+    }
+    return null;
+  }
+  
+
   panelOpenState1 = false;
   panelOpenState2 = false;
 }

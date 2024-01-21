4300ed9316540958f390d79d2f0d7e11ef912ed6
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-01-21 19:59:19 +0100

    channel create upate

--- src/app/mainscreen/workspace/workspace.component.ts
+++ src/app/mainscreen/workspace/workspace.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-workspace',
@@ -6,8 +7,12 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
   styleUrl: './workspace.component.scss'
 })
 export class WorkspaceComponent {
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  channelCreateForm: FormGroup;
+  constructor(private el: ElementRef, private renderer: Renderer2, private formBuilder: FormBuilder) {
+    this.channelCreateForm = this.formBuilder.group({
+      channelName: ['', [Validators.required]]
+    });
+  }
 
   resetSelection() {
     const elements = this.el.nativeElement.querySelectorAll('.selectable');
@@ -40,6 +45,10 @@ export class WorkspaceComponent {
     if (channelCreateWindow && channelCreateContainer) {
       if (!channelCreateContainer.contains(event.target as Node)) {
         channelCreateWindow.style.display = 'none';
+        const inputElements = channelCreateWindow.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
+        inputElements.forEach((inputElement) => {
+          inputElement.value = '';
+        });
       }
     }
   }
@@ -48,13 +57,32 @@ export class WorkspaceComponent {
     const channelCreateWindow = document.querySelector('.channel-create-window') as HTMLElement | null;
     if (channelCreateWindow) {
       channelCreateWindow.style.display = 'none';
+      const inputElements = channelCreateWindow.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
+      inputElements.forEach((inputElement) => {
+        inputElement.value = '';
+      });
     }
   }
+
 
   showChannelCreateWindow(): void {
     const channelCreateWindow = document.querySelector('.channel-create-window') as HTMLElement | null;
     if (channelCreateWindow) {
       channelCreateWindow.style.display = 'block';
+    }
+  }
+
+  channelCreateSecondScreen(): void {
+    const channelCreateWindow = document.querySelector('.channel-create-window') as HTMLElement | null;
+    if (channelCreateWindow) {
+      const firstScreen = channelCreateWindow.querySelectorAll('.first-screen') as NodeListOf<HTMLInputElement>;
+      const secondScreen = channelCreateWindow.querySelectorAll('.second-screen') as NodeListOf<HTMLInputElement>;
+      firstScreen.forEach((firstScreen) => {
+        firstScreen.style.display = 'none';
+      });
+      secondScreen.forEach((secondScreen) => {
+        secondScreen.style.display = 'block';
+      });
     }
   }
 

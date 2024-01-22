9a8be715c377fc0da89de89f8ee3df0d21764a98
Matthias Berthold <matthiasberthold33@gmail.com>
2024-01-22 13:48:06 +0100

    hover functions part 2 channel chat

--- src/app/mainscreen/channel-chat/channel-chat.component.ts
+++ src/app/mainscreen/channel-chat/channel-chat.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-channel-chat',
   templateUrl: './channel-chat.component.html',
-  styleUrl: './channel-chat.component.scss'
+  styleUrl: './channel-chat.component.scss',
+
+  animations: [
+    trigger('slideAndFade', [
+      state('hidden', style({
+        transform: 'translateX(30px)',
+        opacity: 0,
+      })),
+      state('visible', style({
+        transform: 'translateX(-45px)',
+        opacity: 1,
+      })),
+      transition('hidden => visible', animate('100ms ease-out')),
+      transition('visible => hidden', animate('100ms ease-in')),
+    ]),
+
+    trigger('slideAndFadeLeft', [
+      state('hidden', style({
+        transform: 'translateX(-30px)',
+        opacity: 0,
+      })),
+      state('visible', style({
+        transform: 'translateX(45px)',
+        opacity: 1,
+      })),
+      transition('hidden => visible', animate('100ms ease-out')),
+      transition('visible => hidden', animate('100ms ease-in')),
+    ]),
+  ],
 })
 export class ChannelChatComponent {
+  animationState = 'hidden';
+  animationState1 = 'hidden';
 
+  reactions = [
+    { user: 'Noah Braun', count: 1 },
+  ];
+
+  showContainer: boolean[] = [];
+
+    constructor() {
+      this.showContainer = new Array(this.reactions.length).fill(false);
+  }
+  
+
+  showReaction(index: number) {
+    debugger;
+      this.showContainer[index] = true;
+  }
+
+  hideReaction(index: number) {
+      this.showContainer[index] = false;
+  }
+
+
+  toggleAnimationState(state: 'visible' | 'hidden'): void {
+    this.animationState = state;
+  }
+
+  toggleAnimationState1(state: 'visible' | 'hidden'): void {
+    this.animationState1 = state;
+  }
 }

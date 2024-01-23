c1db5a04160ca7d714b6b42625da54c14953154e
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-01-23 16:29:37 +0100

    workspace update

--- src/app/mainscreen/workspace/workspace.component.ts
+++ src/app/mainscreen/workspace/workspace.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2, ChangeDetectorRef } from '@angular/core';
+import { Component, ElementRef, Renderer2, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
@@ -9,87 +9,74 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 
 export class WorkspaceComponent {
+  @ViewChild('channelCreateWindow') channelCreateWindow!: ElementRef<HTMLElement>;
+  @ViewChild('channelCreateContainer') channelCreateContainer!: ElementRef<HTMLElement>;
+
   panelOpenState1 = false;
   panelOpenState2 = false;
-  
-  inputOpen: boolean = false;
+  showInputNames: boolean = false;
   
   channelCreateForm: FormGroup;
-
   body = this.elRef.nativeElement.ownerDocument.body;
 
-  constructor(private elRef: ElementRef, private renderer: Renderer2, private formBuilder: FormBuilder, private cdr: ChangeDetectorRef) {
+  constructor(
+    private elRef: ElementRef, 
+    private renderer: Renderer2, 
+    private formBuilder: FormBuilder, 
+    private cdr: ChangeDetectorRef
+    ) {
     this.channelCreateForm = this.formBuilder.group({
       channelName: ['', [Validators.required]],
       selectedOption: ['specificMembers']
     });
   }
 
-  resetSelection() {
-    const elements = this.elRef.nativeElement.querySelectorAll('.selectable');
-    elements.forEach((element: HTMLElement) => {
-      this.renderer.removeClass(element, 'selected');
-    });
-  }
-
-  handleClick(event: MouseEvent) {
-    this.resetSelection();
+  /**
+  * Handles the click event on selectable elements. Removes the class "selected" from all other elements and sets this class to clicked elements
+  *
+  * @param {MouseEvent} event - The click event.
+  * @returns {void}
+  */
+  handleClick(event: MouseEvent): void {
     const target = event.target as HTMLElement;
-    const selectableElement = this.findClosestSelectable(target);
-    if (selectableElement) {
-      this.renderer.addClass(selectableElement, 'selected');
-    }
+    const selectableElement = this.findParentElement(target);
+    this.elRef.nativeElement.querySelectorAll('.selectable').forEach((element: HTMLElement) => element.classList.remove('selected'));
+    this.renderer.addClass(selectableElement, 'selected');
   }
-  private findClosestSelectable(element: HTMLElement): HTMLElement | null {
-    while (element) {
-      if (element.classList.contains('selectable')) {
-        return element;
-      }
-      element = element.parentElement!;
+  
+  private findParentElement(target: HTMLElement): HTMLElement | null {
+    while (target && !target.classList.contains('selectable')) {
+      target = target.parentElement!;
     }
-    return null;
+    return target;
+  }
+  
+  showChannelCreateWindow(): void {
+      this.channelCreateWindow.nativeElement.style.display = 'block';
+      this.renderer.setStyle(this.body, 'overflow', 'hidden');
   }
 
   hideChannelCreateWindow(event: MouseEvent): void {
-    const channelCreateWindow = document.querySelector('.channel-create-window') as HTMLElement | null;
-    const channelCreateContainer = document.querySelector('.channel-create-container') as HTMLElement | null;
-    if (channelCreateWindow && channelCreateContainer) {
-      if (!channelCreateContainer.contains(event.target as Node)) {
-        channelCreateWindow.style.display = 'none';
-        const inputElements = channelCreateWindow.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
-        inputElements.forEach((inputElement) => {
-          inputElement.value = '';
-        });
+    if (this.channelCreateWindow && this.channelCreateContainer) {
+      if (!this.channelCreateContainer.nativeElement.contains(event.target as Node)) {
+        this.closeChannelCreateWindow();
       }
     }
   }
 
   closeChannelCreateWindow(): void {
-    const channelCreateWindow = document.querySelector('.channel-create-window') as HTMLElement | null;
-    if (channelCreateWindow) {
-      channelCreateWindow.style.display = 'none';
+      this.channelCreateWindow.nativeElement.style.display = 'none';
       this.renderer.setStyle(this.body, 'overflow', 'auto');
-      const inputElements = channelCreateWindow.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
+      const inputElements = this.channelCreateWindow.nativeElement.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
       inputElements.forEach((inputElement) => {
         inputElement.value = '';
       });
-    }
-  }
-
-
-  showChannelCreateWindow(): void {
-    const channelCreateWindow = document.querySelector('.channel-create-window') as HTMLElement | null;
-    if (channelCreateWindow) {
-      channelCreateWindow.style.display = 'block';
-      this.renderer.setStyle(this.body, 'overflow', 'hidden');
-    }
   }
 
   channelCreateSecondScreen(): void {
-    const channelCreateWindow = document.querySelector('.channel-create-window') as HTMLElement | null;
-    if (channelCreateWindow) {
-      const firstScreen = channelCreateWindow.querySelectorAll('.first-screen') as NodeListOf<HTMLInputElement>;
-      const secondScreen = channelCreateWindow.querySelectorAll('.second-screen') as NodeListOf<HTMLInputElement>;
+    if (this.channelCreateWindow) {
+      const firstScreen = this.channelCreateWindow.nativeElement.querySelectorAll('.first-screen') as NodeListOf<HTMLInputElement>;
+      const secondScreen = this.channelCreateWindow.nativeElement.querySelectorAll('.second-screen') as NodeListOf<HTMLInputElement>;
       firstScreen.forEach((firstScreen) => {
         firstScreen.style.display = 'none';
         this.renderer.setStyle(this.body, 'overflow', 'auto');
@@ -102,14 +89,12 @@ export class WorkspaceComponent {
   }
 
   onShowClick() {
-    this.inputOpen = true;
-    // Du könntest dies hinzufügen, um sicherzustellen, dass die Ansicht aktualisiert wird
+    this.showInputNames = true;
     this.cdr.detectChanges();
   }
 
   onHideClick() {
-    this.inputOpen = false;
-    // Auch hier kannst du dies hinzufügen, um sicherzustellen, dass die Ansicht aktualisiert wird
+    this.showInputNames = false;
     this.cdr.detectChanges();
   }
 

914ee8c1c844b5c67d120b8929b8d9965d9b6f76
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-01-28 20:45:44 +0100

    workspace channel create update

--- src/app/mainscreen/workspace/workspace.component.ts
+++ src/app/mainscreen/workspace/workspace.component.ts
@@ -1,26 +1,10 @@
-import {
-  Component,
-  ElementRef,
-  Renderer2,
-  ChangeDetectorRef,
-  ViewChild,
-  inject,
-} from '@angular/core';
+import { Component, ElementRef, Renderer2, ChangeDetectorRef, ViewChild, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {
-  Firestore,
-  Unsubscribe,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  onSnapshot,
-} from '@angular/fire/firestore';
+import { Firestore, Unsubscribe, collection, doc, getDoc, onSnapshot } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../../models/user.class';
 import { ChannelService } from '../../services/channel.service';
 import { ChannelDataService } from '../../services/channel-data.service';
-import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-workspace',
@@ -28,10 +12,8 @@ import { AuthService } from '../../auth.service';
   styleUrl: './workspace.component.scss',
 })
 export class WorkspaceComponent {
-  @ViewChild('channelCreateWindow')
-  channelCreateWindow!: ElementRef<HTMLElement>;
-  @ViewChild('channelCreateContainer')
-  channelCreateContainer!: ElementRef<HTMLElement>;
+  @ViewChild('channelCreateWindow')channelCreateWindow!: ElementRef<HTMLElement>;
+  @ViewChild('channelCreateContainer')channelCreateContainer!: ElementRef<HTMLElement>;
   panelOpenState1 = false;
   panelOpenState2 = false;
 
@@ -39,9 +21,13 @@ export class WorkspaceComponent {
   userID: any;
   allUsers: User[] = [];
   userFullName: String = '';
-  
-  selectedUsers: User[] = [];
 
+  selectedUsers: User[] = [];
+  searchQuery: string = '';
+
+  isChannelCreateWindow: boolean = false;
+  isFirstScreen: boolean = true;
+  isSecondScreen: boolean = false;
   showInputNames: boolean = false;
 
   channelCreateForm: FormGroup;
@@ -71,60 +57,87 @@ export class WorkspaceComponent {
     this.userList = this.getUserfromFirebase();
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     if (this.userID) {
       this.checkIsGuestLogin();
     }
     this.getUserList();
-    this.getAllChannel();
-  }
-
-  private getUserList(): void {
-    this.unsubUser = onSnapshot(this.channelService.getUsersRef(), (list) => {
-      this.allUsers = [];
-      list.forEach((singleUser) => {
-        let user = new User(singleUser.data());
-        user.id = singleUser.id;
-        this.allUsers.push(user);
-      });
-    });
-  }
-
-  private async getAllChannel(): Promise<void> {
-    await this.getChannelList();
-  }
-  
-
-  getProfileImagePath(user: User): string {
-      if (user.profileImg.startsWith('https://firebasestorage.googleapis.com')) {
-        return user.profileImg;
-      } else {
-        return `./assets/img/${user.profileImg}`;
-      }
+    await this.getAllChannel();
   }
 
   ngOnDestroy() {
     if (this.unsubscribeSnapshot) {
       this.unsubscribeSnapshot();
     }
+    if (this.unsubUser) {
+      this.unsubUser();
+    }
     if (this.userID) {
       this.checkIsGuestLogin();
     }
+    this.getUserList();
+  }
 
+  /**
+   * Retrieve and update the user list using Firestore snapshot.
+   */
+  private getUserList(): void {
     this.unsubUser = onSnapshot(this.channelService.getUsersRef(), (list) => {
-      this.allUsers = [];
-      list.forEach((singleUser) => {
-        let user = new User(singleUser.data());
-        user.id = singleUser.id;
-        this.allUsers.push(user);
-      });
+      this.updateAllUsersList(list);
     });
   }
 
+  /**
+   * Update the allUsers array based on the provided Firestore snapshot.
+   *
+   * @param {any} list - Firestore snapshot of user data.
+   */
+  private updateAllUsersList(list: any): void {
+    this.allUsers = [];
+    list.forEach((singleUser: { data: () => any; id: string }) => {
+      let user = new User(singleUser.data());
+      user.id = singleUser.id;
+      this.allUsers.push(user);
+    });
+  }
+
+  /**
+   * Retrieve and update the channel list.
+   *
+   * @returns {Promise<void>} A Promise that resolves after retrieving the channel list.
+   */
+  private async getAllChannel(): Promise<void> {
+    await this.getChannelList();
+  }
+
+  /**
+   * Get the profile image path for a user.
+   *
+   * @param {User} user - The user object.
+   * @returns {string} The profile image path.
+   */
+  getProfileImagePath(user: User): string {
+    if (user.profileImg.startsWith('https://firebasestorage.googleapis.com')) {
+      return user.profileImg;
+    } else {
+      return `./assets/img/${user.profileImg}`;
+    }
+  }
+
+  /**
+   * Get the document reference for the user.
+   *
+   * @returns {any} The document reference.
+   */
   getUserID() {
     return doc(collection(this.firestore, 'users'), this.userID);
   }
 
+  /**
+   * Retrieve user data from Firebase and update the user object.
+   *
+   * @returns {Promise<void>} A Promise that resolves after retrieving user data.
+   */
   async getUserfromFirebase(): Promise<void> {
     try {
       const userDocRef = doc(this.firestore, 'users', this.userID);
@@ -138,12 +151,19 @@ export class WorkspaceComponent {
     } catch (error) {}
   }
 
+  /**
+   * Retrieve and update the channel list.
+   *
+   * @returns {Promise<void>} A Promise that resolves after retrieving the channel list.
+   */
   async getChannelList(): Promise<void> {
     this.channels = await this.channelService.getAllChannels();
     console.log('Channels:', this.channels);
-    // Hier kannst du weitere Aktionen mit den Kanälen durchführen
   }
 
+  /**
+   * Check if the user is logged in as a guest and update user information accordingly.
+   */
   checkIsGuestLogin(): void {
     getDoc(this.getUserID()).then((docSnapshot) => {
       if (docSnapshot.exists()) {
@@ -156,7 +176,9 @@ export class WorkspaceComponent {
   }
 
   /**
-   * Handles the click event on selectable elements. Removes the class "selected" from all other elements and sets this class to clicked elements
+   * Handles the click event on selectable elements.
+   * Removes the class "selected" from all other elements
+   * and sets this class to clicked elements
    *
    * @param {MouseEvent} event - The click event.
    * @returns {void}
@@ -170,6 +192,12 @@ export class WorkspaceComponent {
     this.renderer.addClass(selectableElement, 'selected');
   }
 
+  /**
+   * Find the parent element with the class 'selectable'.
+   *
+   * @param {HTMLElement} target - The target element.
+   * @returns {HTMLElement | null} The parent element or null if not found.
+   */
   private findParentElement(target: HTMLElement): HTMLElement | null {
     while (target && !target.classList.contains('selectable')) {
       target = target.parentElement!;
@@ -177,78 +205,64 @@ export class WorkspaceComponent {
     return target;
   }
 
-  showChannelCreateWindow(): void {
-    this.channelCreateWindow.nativeElement.style.display = 'block';
-    this.renderer.setStyle(this.body, 'overflow', 'hidden');
-  }
-
-  hideChannelCreateWindow(event: MouseEvent): void {
-    if (this.channelCreateWindow && this.channelCreateContainer) {
-      if (
-        !this.channelCreateContainer.nativeElement.contains(
-          event.target as Node
-        )
-      ) {
-        this.closeChannelCreateWindow();
-      }
+  /**
+   * Display the channel creation window.
+   */
+  toggleChannelCreateWindow() {
+    this.isChannelCreateWindow = !this.isChannelCreateWindow;
+    if (this.isChannelCreateWindow) {
+      this.renderer.setStyle(this.body, 'overflow', 'hidden');
+    } else {
+      this.renderer.setStyle(this.body, 'overflow', 'auto');
     }
   }
-
-  closeChannelCreateWindow(): void {
-    this.channelCreateWindow.nativeElement.style.display = 'none';
-    this.renderer.setStyle(this.body, 'overflow', 'auto');
-    const inputElements =
-      this.channelCreateWindow.nativeElement.querySelectorAll(
-        'input'
-      ) as NodeListOf<HTMLInputElement>;
-    inputElements.forEach((inputElement) => {
-      inputElement.value = '';
-    });
+  
+  /**
+   * Navigate to the second screen of channel creation.
+   */
+  toggleChannelCreateContainer() {
+    this.isFirstScreen = !this.isFirstScreen;
+    this.isSecondScreen= !this.isSecondScreen;
   }
 
-  channelCreateSecondScreen(): void {
-    if (this.channelCreateWindow) {
-      const firstScreen =
-        this.channelCreateWindow.nativeElement.querySelectorAll(
-          '.first-screen'
-        ) as NodeListOf<HTMLInputElement>;
-      const secondScreen =
-        this.channelCreateWindow.nativeElement.querySelectorAll(
-          '.second-screen'
-        ) as NodeListOf<HTMLInputElement>;
-      firstScreen.forEach((firstScreen) => {
-        firstScreen.style.display = 'none';
-        this.renderer.setStyle(this.body, 'overflow', 'auto');
-      });
-      secondScreen.forEach((secondScreen) => {
-        secondScreen.style.display = 'block';
-        this.renderer.setStyle(this.body, 'overflow', 'hidden');
-      });
-    }
-  }
-
+  /**
+   * Show input names on button click.
+   */
   onShowClick() {
     this.showInputNames = true;
     this.cdr.detectChanges();
   }
 
+  /**
+   * Hide input names on button click.
+   */
   onHideClick() {
     this.showInputNames = false;
     this.cdr.detectChanges();
   }
 
-  searchQuery: string = '';
-
+  /**
+   * Search input change event handler.
+   *
+   * @param {any} event - The input change event.
+   */
   onSearchInputChange(event: any): void {
     this.searchQuery = event.target.value;
+    if (this.searchQuery.trim() !== '') {
+      this.filterUsers();
+    }
   }
 
+  /**
+   * Filter users based on the search query.
+   *
+   * @returns {User[]} The filtered user array.
+   */
   filterUsers(): User[] {
     const trimmedQuery = this.searchQuery.trim().toLowerCase();
     if (!trimmedQuery) {
       return this.allUsers;
     }
-
     return this.allUsers.filter(
       (user) =>
         user.firstname.toLowerCase().includes(trimmedQuery) ||
@@ -256,19 +270,32 @@ export class WorkspaceComponent {
     );
   }
 
-
+  /**
+   * Select a user and update the selected users input.
+   *
+   * @param {User} user - The user to be selected.
+   */
   selectUser(user: User): void {
     if (!this.selectedUsers.includes(user)) {
       this.selectedUsers.push(user);
       this.updateSelectedUsersInput();
+      this.searchQuery = '';
     }
   }
 
+  /**
+   * Remove a user and update the selected users input.
+   *
+   * @param {User} user - The user to be removed.
+   */
   removeUser(user: User): void {
     this.selectedUsers = this.selectedUsers.filter((u) => u !== user);
     this.updateSelectedUsersInput();
   }
 
+  /**
+   * Update the input value with the names of selected users.
+   */
   updateSelectedUsersInput(): void {
     const selectedUsersNames = this.selectedUsers
       .map((u) => `${u.firstname} ${u.lastname}`)
@@ -277,6 +304,4 @@ export class WorkspaceComponent {
       .get('selectedUsersInput')
       ?.setValue(selectedUsersNames);
   }
-
-  
 }

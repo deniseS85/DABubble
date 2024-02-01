9ddd74c6553e85bc7f72633cab789162993ddfe8
deniseS85 <denisabel78@hotmail.de>
2024-02-01 09:38:06 +0100

    change style in channel

--- src/app/models/channel.interface.ts
+++ src/app/models/channel.interface.ts
@@ -11,24 +11,3 @@ export interface Channel{
     channelCreator: string;
 }
 
-export interface Chat {
-    avatar: string;
-    reactionMenu: {
-      emoji: string;
-      handsUp: string;
-      addReaction: string;
-      answer: string;
-      isEmojiOpen: boolean;
-      selectedEmojis: string[];
-    };
-    userName: string;
-    chatUserID: string;
-    sendingTime: string;
-    messageContent: string;
-    answerInfo: {
-      counter: number;
-      lastAnswerTime: string;
-    };
-    date: string;
-    animationState?: 'visible' | 'hidden';
-}

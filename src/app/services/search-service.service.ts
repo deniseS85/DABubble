9b291855521f22a1730524de87abfb176c3f7831
Matthias Berthold <matthiasberthold33@gmail.com>
2024-02-07 07:29:51 +0100

    searchfield header search service implented

--- src/app/services/search-service.service.ts
+++ src/app/services/search-service.service.ts
@@ -1,42 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Channel } from '../models/channel.class';
 import { Message } from "../models/message.interface";
-
+import { ChannelService } from "../services/channel.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
+  private channels: Channel[] = []; // Aktuelle Kanaldaten speichern
 
-  constructor() { }
+  constructor(private channelService: ChannelService) { 
+    // Kanäle beim Initialisieren des Dienstes laden
+    this.loadChannels();
+  }
+
+  private async loadChannels(): Promise<void> {
+    try {
+      // Kanäle aus Firestore abrufen
+      const firestoreChannels = await this.channelService.getAllChannels();
+      // Mapping der Firestore-Kanäle auf Channel-Objekte
+      this.channels = firestoreChannels.map((data: any) => new Channel(data));
+    } catch (error) {
+      console.error('Fehler beim Laden der Kanäle:', error);
+    }
+  }
 
   searchChannels(query: string): Channel[] {
-    // Hier führst du die Suchlogik für Kanäle durch
-    // Dummy-Daten für die Suche
-    const channels: Channel[] = [
-      new Channel({ channelID: '1', channelDescription: 'Channel 1 Description', channelname: 'Channel 1', channelUsers: [], channelCreator: '', users: [] }),
-      new Channel({ channelID: '2', channelDescription: 'Channel 2 Description', channelname: 'Channel 2', channelUsers: [], channelCreator: '', users: [] }),
-      new Channel({ channelID: '3', channelDescription: 'El Nuevo Canalo Description', channelname: 'El Nuevo Canalo', channelUsers: [], channelCreator: '', users: [] })
-    ];
-
-    // Hier kannst du die Dummy-Daten durchsuchen und passende Kanäle zurückgeben
-    const matchingChannels: Channel[] = channels.filter(channel => channel.channelname.includes(query));
-
+    // Hier kannst du die aktuellen Kanäle durchsuchen
+    const matchingChannels: Channel[] = this.channels.filter(channel => channel.channelname.toLowerCase().includes(query.toLowerCase()));
     return matchingChannels;
   }
 
   searchMessages(query: string): Message[] {
-    // Hier führst du die Suchlogik für Nachrichten durch
     // Dummy-Daten für die Suche
     const messages: Message[] = [
       { senderID: '1', senderNamen: 'Sender 1', sendTime: new Date(), messageText: 'Message 1', messageID: '1' },
       { senderID: '2', senderNamen: 'Sender 2', sendTime: new Date(), messageText: 'Message 2', messageID: '2' },
-      
     ];
 
     // Hier kannst du die Dummy-Daten durchsuchen und passende Nachrichten zurückgeben
     const matchingMessages: Message[] = messages.filter(message => message.messageText.includes(query));
-
     return matchingMessages;
   }
 }

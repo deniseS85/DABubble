b1c65efca491bdadc4bbcff1eca99d0195da02e5
Matthias Berthold <matthiasberthold33@gmail.com>
2024-02-12 10:04:29 +0100

    searchfield header rendering list

--- src/app/services/channel-data.service.ts
+++ src/app/services/channel-data.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { Channel } from '../models/channel.class';
-import { Firestore, Unsubscribe, docData, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, Unsubscribe, collectionData, docData, onSnapshot } from '@angular/fire/firestore';
 import { ChannelService } from './channel.service';
-import { Observable, Subject, map } from "rxjs";
+import { Observable, Subject, Subscription, map } from "rxjs";
 import { query, getDocs, collection, doc, DocumentReference, DocumentData, QuerySnapshot, CollectionReference } from 'firebase/firestore';
 import { AuthService } from './auth.service';
 
@@ -22,47 +22,50 @@ export class ChannelDataService {
   private highlightUserSubject = new Subject<string>();
   highlightUser$ = this.highlightUserSubject.asObservable();
   unsubChannelUser: Unsubscribe | undefined;
+  items$: Observable<(DocumentData | (DocumentData & {}))[]> | undefined;
+  items: Subscription | undefined;
 
 
   constructor(
-    private channelService: ChannelService
+    private channelService: ChannelService,
   ) {
     this.loadFirstChannelID().then(() => {
       this.subscribeToChannelUpdates();
-  });
+    });
+    this.updateChannelInfo();
   }
 
   ngOnDestroy() {
     if (this.unsubChannelUser) {
       this.unsubChannelUser();
     }
-
   }
 
   async loadFirstChannelID() {
-      let channels = await this.channelService.getAllChannels();
-      if (channels.length > 0) {
-          this.channelID = channels[0].channelID;
-      }
+    let channels = await this.channelService.getAllChannels();
+    if (channels.length > 0) {
+      this.channelID = channels[0].channelID;
+    }
   }
 
   private subscribeToChannelUpdates() {
-      const channelRef = this.channelService.getSingleChannel(this.channelID);
+    const channelRef = this.channelService.getSingleChannel(this.channelID);
 
-      this.unsubChannelUser = onSnapshot(channelRef, (snapshot) => {
-          const channel = snapshot.data();
-          let channelInfo = new Channel(channel);
-          this.channelName = channelInfo.channelname;
-          this.channelUsers = channelInfo.channelUsers;
-          this.channelCreator = channelInfo.channelCreator;
-          this.channelDescription = channelInfo.channelDescription;
-          this.channelID = channelInfo.channelID;
-          this.changeSelectedChannel(channelInfo.channelname, channelInfo.channelCreator, channelInfo.channelDescription);
-      });
+    this.unsubChannelUser = onSnapshot(channelRef, (snapshot) => {
+      const channel = snapshot.data();
+      let channelInfo = new Channel(channel);
+      this.channelName = channelInfo.channelname;
+      this.channelUsers = channelInfo.channelUsers;
+      this.channelCreator = channelInfo.channelCreator;
+      this.channelDescription = channelInfo.channelDescription;
+      this.channelID = channelInfo.channelID;
+      this.changeSelectedChannel(channelInfo.channelname, channelInfo.channelCreator, channelInfo.channelDescription);
+      console.log(channelInfo.channelID);
+    });
   }
-  
 
-  async changeSelectedChannel(selectedChannelName: string, selectedChannelCreator: string, selectedChannelDescription: string ) {
+
+  async changeSelectedChannel(selectedChannelName: string, selectedChannelCreator: string, selectedChannelDescription: string) {
     this.channelName = selectedChannelName;
     this.channelDescription = selectedChannelDescription;
     this.channelCreator = selectedChannelCreator;
@@ -71,6 +74,17 @@ export class ChannelDataService {
 
   highlightUserInWorkspace(userFullName: string): void {
     this.highlightUserSubject.next(userFullName);
+  }
+
+  async updateChannelInfo() {
+    this.items$ = collectionData(this.channelService.getChannelRef());
+    this.items = this.items$.subscribe( (list: any) => {
+      list.forEach( (channelItem: any) => {
+        if(this.channelID == channelItem.channelID) {
+          console.log('item$', channelItem.channelID);
+        }
+      });
+    });
   }
 
 }
